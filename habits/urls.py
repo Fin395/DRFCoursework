0df2d2a27c8b2ffa8449078953734da9e52a826c
Sergei Finogenov <serfinogenov@yandex.ru>
2025-08-10 05:35:56 +0300

    creating paginator; adding view for public habits list

--- habits/urls.py
+++ habits/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from habits.apps import HabitsConfig
-from habits.views import HabitViewSet
+from habits.views import HabitViewSet, PublicHabitListAPIView
 
 app_name = HabitsConfig.name
 
 router = DefaultRouter()
 router.register(r"habit", HabitViewSet, basename="habit")
 urlpatterns = [
-    # path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
+    path('public-habit/', PublicHabitListAPIView.as_view(), name="public-habit-list"),
     # path("lesson/", LessonListAPIView.as_view(), name="lesson-list"),
     # path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson-get"),
     # path(
--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
+
 
 from habits.models import Habit
+from habits.paginators import HabitPagination
 from habits.serializers import HabitSerializer
 from rest_framework.permissions import IsAuthenticated
 
@@ -9,7 +12,7 @@ from users.permissions import IsOwner
 
 class HabitViewSet(ModelViewSet):
     serializer_class = HabitSerializer
-    # pagination_class = CustomPagination
+    pagination_class = HabitPagination
 
     def perform_create(self, serializer):
         serializer.validated_data['user'] = self.request.user
@@ -18,7 +21,7 @@ class HabitViewSet(ModelViewSet):
         serializer.save()
 
     def get_queryset(self):
-        return Habit.objects.filter(user=self.request.user) and Habit.objects.filter(is_public=True)
+        return Habit.objects.filter(user=self.request.user)
 
     def get_permissions(self):
         self.permission_classes = []
@@ -28,3 +31,12 @@ class HabitViewSet(ModelViewSet):
             self.permission_classes = [IsAuthenticated, IsOwner]
 
         return [permission() for permission in self.permission_classes]
+
+
+class PublicHabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = HabitPagination
+    permission_classes = IsAuthenticated
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)
